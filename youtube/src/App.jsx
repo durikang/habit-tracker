e8048e7a69ba5bfeb9865ed819e242aec2128403
duri <kooda21@naver.com>
2020-12-24 15:32:41 +0900

    Commit

--- youtube/src/App.jsx
+++ youtube/src/App.jsx
@@ -1,8 +1,29 @@
+import { useEffect,useState } from 'react';
 import './App.css';
+import Video_list from './components/video_list/Video_list';
+
+
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos/?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAXZM9JYij_pnd2acWUtGPn5nVWUaYqt98", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+
+  }, []); 
+
+  //mount가 돼었을때만 위에 함수 호출
+
   return (
-    <h1>Hello</h1>
+    <Video_list videos={videos} />
   );
 }
 
---
+++ youtube/src/components/video_item/Video_item.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Video_item = (props) => (
+    <p>{props.video.snippet.title}</p>          
+);
+
+export default Video_item;

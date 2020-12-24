8a6789425caf43acd55ed5090d70227c5c65ef8e
duri <kooda21@naver.com>
2020-12-24 10:37:15 +0900

    새 프로젝트 준비

---
+++ t-test/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Button1 from './component/Button1';
+import Button2 from './component/Button2';
+
+function App() {
+  return (
+    <>
+      <Button1 />
+      <Button2 />
+    </>
+  );
+}
+
+export default App;
---
+++ t-test/src/component/Button1.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import styles from './Button1.module.css';
+class Button1 extends Component {
+    render() {
+        return (
+            <div className={styles.button}>
+                <span className={styles.text}>Button1</span>
+            </div>
+        );
+    }
+}
+
+export default Button1;

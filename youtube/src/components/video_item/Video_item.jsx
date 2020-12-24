2ca944b82d8fe7517989af33a1742ad37868e148
duri <kooda21@naver.com>
2020-12-24 15:59:17 +0900

    commit

--- youtube/src/components/video_item/Video_item.jsx
+++ youtube/src/components/video_item/Video_item.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import styles from './video_item.module.css'
+//만약 넘겨오는 파라미터의 이름을 정의하고 싶을 때
+// props대신 props다음 파라미터로 넘겨오는 인자 이름
+// 아래를 예를 들면 props.video 니까 {video} 로
+// const Video_item = ({video}) => ( ..중략 ..)
+// 혹은 {video : videoItem } => ( ..etc ..)
 
-const Video_item = (props) => (
-    <p>{props.video.snippet.title}</p>          
+
+const Video_item = ({video: { snippet}}) => (
+    <li className={styles.container}>
+        <div className={styles.video}>
+            <img  className={styles.thumbnail}src={snippet.thumbnails.medium.url} alt = "video thumbnail"/>
+            <div className={styles.metadata}>
+                <p className={styles.title}>{snippet.title}</p>
+                <p className={styles.channel}>{snippet.channelTitle}</p>
+            </div>
+        </div>
+    </li>
 );
 
 export default Video_item;
--- youtube/src/components/video_list/Video_list.jsx
+++ youtube/src/components/video_list/Video_list.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Video_item from '../video_item/Video_item';
+import styles from './video_list.module.css';
 
 const Video_list = (props) => (
-  <ul>
+  <ul className={styles.videos}>
       {props.videos.map(video => <Video_item key={video.id} video={video}/>)}
   </ul>          
 );

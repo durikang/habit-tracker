eefa1178a61c664472f679f9313babf985638a84
duri <kooda21@naver.com>
2021-01-06 13:26:12 +0900

    CRUD UPDATE

--- new-card-maker/src/components/maker/Maker.jsx
+++ new-card-maker/src/components/maker/Maker.jsx
@@ -7,41 +7,46 @@ import Preview from '../preview/Preview';
 import styles from './Maker.module.css';
 
 const Maker = ({ authService }) => {
-  const [cards, setCards] = useState([
-    {
-      id: '1',
-      name: '강한별',
-      company: 'Samsung',
-      theme: 'dark',
-      title: 'Software Engineer',
-      email: 'ellie@gmail.com',
-      message: 'go for it',
-      fileName: 'ellie',
-      fileURL: null,
-    },
-    {
-      id: '2',
-      name: '석연우',
-      company: 'Samsung',
-      theme: 'light',
-      title: 'Software Engineer',
-      email: 'ellie@gmail.com',
-      message: 'go for it',
-      fileName: 'ellie',
-      fileURL: 'ellie.png',
-    },
-    {
-      id: '3',
-      name: '조찬희',
-      company: 'Samsung',
-      theme: 'colorful',
-      title: 'Software Engineer',
-      email: 'ellie@gmail.com',
-      message: 'go for it',
-      fileName: 'ellie',
-      fileURL: null,
-    },
-  ]);
+  const [cards, setCards] = useState({
+      1:{
+        id: '1',
+        name: '강한별',
+        company: 'Samsung',
+        theme: 'dark',
+        title: 'Software Engineer',
+        email: 'ellie@gmail.com',
+        message: 'go for it',
+        fileName: 'ellie',
+        fileURL: null,
+      },2:{
+        id: '2',
+        name: '석연우',
+        company: 'Samsung',
+        theme: 'light',
+        title: 'Software Engineer',
+        email: 'ellie@gmail.com',
+        message: 'go for it',
+        fileName: 'ellie',
+        fileURL: 'ellie.png',
+      },3:
+      {
+        id: '3',
+        name: '조찬희',
+        company: 'Samsung',
+        theme: 'colorful',
+        title: 'Software Engineer',
+        email: 'ellie@gmail.com',
+        message: 'go for it',
+        fileName: 'ellie',
+        fileURL: null,
+      }
+
+
+
+
+  });
+
+
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
@@ -54,17 +59,26 @@ const Maker = ({ authService }) => {
       }
     });
   });
-
-  const addCard = card => {
-    const updated = [...cards, card];
-    setCards(updated);
+  const createOrupdateCard = card => {
+    setCards(cards =>{
+      const updated = {...cards};
+      updated[card.id] = card;  
+      return updated;
+    });
+  };
+  const deleteCard = card => {
+    setCards(cards =>{
+      const updated = {...cards};
+      delete updated[card.id];  
+      return updated;
+    });
   };
 
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} addCard={addCard} />
+        <Editor cards={cards} addCard={createOrupdateCard} updateCard={createOrupdateCard} deleteCard={deleteCard} />
         <Preview cards={cards} />
       </div>
       <Footer/>
